9c253bd5ae1ab94e69a2815f3ae18a2ca8e2fde6
Ramin Samadi <ramin.samadi@learnet.se>
2022-08-26 15:36:48 +0200

    forsta lektion med javascript

---
+++ test.js
@@ -0,0 +1,40 @@
+console.log("Hej"); //sträng
+console.log(2); //nummer
+console.log(true); //boolean
+console.log(false); //boolean
+
+console.log(1+2); //addition
+console.log(1-2); //subtraktion
+console.log(1*2); //multiplikation
+console.log(1/2); //division
+
+var namn = "Ramin";
+console.log(namn);
+
+namn = "Samadi";
+console.log(namn);
+
+console.log("Hello World");
+
+console.log("Once upon a time");
+console.log("I was falling in love");
+console.log("Now I'm only falling apart");
+
+console.log("Once upon a time \nI was falling in love \nNow I'm only falling apart");
+
+var message = "Live and Sleep!";
+console.log(message);
+
+var username = "Ada Lovelace";
+console.log("Hej", username + "!");
+
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let usr = prompt("Enter your name: ");
+console.log(usr);
+
+var promptSync = require("prompt-sync");
+var promptSync = promptSync();
+
+let 
