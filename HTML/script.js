eaaf619c15df23047ca9f13a01be9540c5c40d55
raminsamadi123 <ramin.samadi@elev.ga.ntig.se>
2023-03-20 18:39:07 +0100

    Worked with dynamic questions for my website

--- HTML/script.js
+++ HTML/script.js
@@ -1,10 +1,31 @@
-function onButton1Click() {
-  const h1MainTitle = document.getElementById("mainTitle");
-  const inputField = document.getElementById("inputField");
-  h1MainTitle.textContent = inputField.value;
+function removeStartQuizButton() {
+  const startQuizButton = document.getElementById("startQuiz");
+  if (startQuizButton) {
+    startQuizButton.remove();
+  };
 };
 
-function onButton2Click() {
-  const age = parseInt(prompt("How old are you?"));
-  alert(`You are ${age} years old!`);
+function startQuiz() {
+  removeStartQuizButton();
+  addQuestion("Är solen varm?", "question1", "Alternativ 1");
+  addQuestion("Är månen kall?", "question2", "Alternativ 1");
 };
+
+function addQuestion(question, name, choice) {
+  const container = document.getElementById("container");
+  
+  const h3 = document.createElement("h3");
+  h3.textContent = question
+  
+  const input = document.createElement("input");
+  input.type = "radio";
+  input.name = name;
+
+  const label = document.createElement("label")
+  label.textContent = choice
+  label.name = name
+
+  container.appendChild(h3);
+  container.appendChild(input);
+  container.appendChild(label);
+}
