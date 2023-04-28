87cd860483cc474b984518f931606b452821d85a
raminsamadi123 <ramin.samadi+github@elev.ga.ntig.se>
2023-04-28 15:08:16 +0200

    changed login() function to use a for loop and users to be an array

--- HTML/login/script.js
+++ HTML/login/script.js
@@ -13,16 +13,18 @@ function login() {
   const username = document.getElementById("username").value; // Gets the value from the input element with username as its ID
   const password = document.getElementById("password").value; // Gets the value from the input element with password as its ID
 
-  const users = { // An arrary containing all registered usernames and passwords
-    "Mohammad": "L8xisa-a#4", // Format: Username: Password;
-    "Ramin": "kjyVv&d2R#2u%D"
-  };
+  const users = [
+    { "username": "Mohammad", "password": "L8xisa-a#4" }, // Mohammad's account
+    { "username": "Ramin", "password": "kjyVv&d2R#2u%D" } // Ramins's account
+  ];
 
-  if (users.hasOwnProperty(username) && users[username] === password) { // Does users contain username and is username's property equal to password?
-    location.href = "/HTML/testIndex.html"
-  } else { // Both of the conditions above were false
-    alert("Wrong username or password. Try again.");
+  for (let i = 0; i < users.length; i++) { // Loop through the array "users" until users.length
+    if (username == users[i].username && password == users[i].password) { // If the user's username is in the array "users" and password is in the array "users"
+      location.href = "/HTML/testIndex.html"; // Redirect client to "/HTML/testIndex.html"
+      return; // Stop the function
+    };
   };
+  alert("Wrong username or password. Try again."); // Once the for loop has been executed then alert
 };
 
 function signup() {
